76147b9169c85e11e0f9abf4af8a86f48425ecc3
judinilson <judinilson0000@gmail.com>
2020-03-31 22:42:35 +0400

    upd grammar,lexer,parser,interpret,with power opertion

--- basic/basic.py
+++ basic/basic.py
@@ -90,7 +90,7 @@ class Position:
 
 
 #######################
-# TOKENS
+# TODO: TOKENS
 #######################
 
 TT_INT = 'TT_INT'
@@ -99,6 +99,7 @@ TT_PLUS = 'PLUS'
 TT_MINUS = 'MINUS'
 TT_MUL = 'MUL'
 TT_DIV = 'DIV'
+TT_POW = 'POW'
 TT_LPAREN = 'LPAREN'
 TT_RPAREN = 'RPAREN'
 TT_EOF = 'EOFP'
@@ -158,6 +159,9 @@ class Lexer:
                 self.advance()
             elif self.current_char == '/':
                 tokens.append(Token(TT_DIV, pos_start=self.pos))
+                self.advance()
+            elif self.current_char == '^':
+                tokens.append(Token(TT_POW, pos_start=self.pos))
                 self.advance()
             elif self.current_char == '(':
                 tokens.append(Token(TT_LPAREN, pos_start=self.pos))
@@ -284,20 +288,14 @@ class Parser:
             ))
         return res
 
-    def factor(self):
+    ##################################################
+    def atom(self):
         res = ParseResult()
         tok = self.current_tok
 
-        if tok.type in (TT_PLUS, TT_MINUS):
-            res.register(self.advance())
-            factor = res.register(self.factor())  # call factor recursively
-            if res.error: return res
-            return res.success(UnaryOpNode(tok, factor))
-
-        elif tok.type in (TT_INT, TT_FLOAT):
+        if tok.type in (TT_INT, TT_FLOAT):
             res.register(self.advance())
             return res.success(NumberNode(tok))
-
         elif tok.type == TT_LPAREN:  # verifying if is left parenthese
             res.register(self.advance())
             expr = res.register(self.expr())
@@ -309,10 +307,25 @@ class Parser:
                 return res.failure(InvalidSyntaxError(
                     self.current_tok.pos_start, self.current_tok.pos_end, "Expected ')'"
                 ))
-
         return res.failure(InvalidSyntaxError(
-            tok.pos_start, tok.pos_end, "Expected int or float"
+            tok.pos_start, tok.pos_end,
+            "Expected int , float, '+' , '-' or '(' "
         ))
+
+    def power(self):
+        return self.bin_op(self.atom, (TT_POW), self.factor)
+
+    def factor(self):
+        res = ParseResult()
+        tok = self.current_tok
+
+        if tok.type in (TT_PLUS, TT_MINUS):
+            res.register(self.advance())
+            factor = res.register(self.factor())  # call factor recursively
+            if res.error: return res
+            return res.success(UnaryOpNode(tok, factor))
+
+        return self.power()
 
     def term(self):
         return self.bin_op(self.factor, (TT_MUL, TT_DIV))
@@ -320,15 +333,17 @@ class Parser:
     def expr(self):
         return self.bin_op(self.term, (TT_PLUS, TT_MINUS))
 
-    def bin_op(self, func, ops):
+    def bin_op(self, func_a, ops, func_b=None):
+        if func_b == None:
+            func_b = func_a
         res = ParseResult()
-        left = res.register(func())
+        left = res.register(func_a())
         if res.error: return res
 
         while self.current_tok.type in ops:
             op_tok = self.current_tok
             res.register(self.advance())
-            right = res.register(func())
+            right = res.register(func_b())
             if res.error: return res
             left = BinOpNode(left, op_tok, right)
         return res.success(left)
@@ -376,7 +391,7 @@ class Number:  # storing number class
         return self
 
     def added_to(self, other):
-        # Return true if the object argument is an instance of the classinfo
+        # Return true if the object argument is an instance of the classinfo(mean its check if the value a num
         # argument, or of a (direct, indirect or virtual) subclass thereof
         if isinstance(other, Number):
             return Number(self.value + other.value).set_context(self.context), None
@@ -398,6 +413,11 @@ class Number:  # storing number class
                     self.context
                 )
             return Number(self.value / other.value).set_context(self.context), None
+
+    def powed_by(self, other):
+        if isinstance(other, Number):
+            return Number(self.value ** other.value).set_context(
+                self.context), None
 
     def __repr__(self):
         return str(self.value)  # convert to string
@@ -450,6 +470,8 @@ class Interpreter:
             result, error = left.multed_by(right)
         elif node.op_tok.type == TT_DIV:
             result, error = left.dived_by(right)
+        elif node.op_tok.type == TT_POW:
+            result, error = left.powed_by(right)
 
         if error:
             return res.failure(error)
